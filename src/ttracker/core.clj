db2781367e70562dcc48c6f23b86e9cdb2e5e290
anuragpeshne <anurag.peshne@gmail.com>
2018-01-09 23:20:17 -0500

    osx notifier added

--- src/ttracker/core.clj
+++ src/ttracker/core.clj
@@ -5,3 +5,19 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn osx-notify
+  ([message, title]           (osx-notify message title "")) ; blank subtitle
+  ([message, title, subtitle] (osx-notify message title subtitle "Glass"))
+  ([message, title, subtitle, sound-name]
+   (..
+    Runtime
+    getRuntime
+    (exec (into-array
+           ["osascript", "-e",
+            (str
+             "display notification \"" message
+             "\" with title \""        title
+             "\" subtitle \""          subtitle
+             "\" sound name \""        sound-name
+             "\"")])))))
