e58e98f94d915dbf90617fa218051fcd7617d170
anuragpeshne <anurag.peshne@gmail.com>
2018-01-10 16:47:03 -0500

    basic pomodoro with osx notification implemented

--- src/ttracker/core.clj
+++ src/ttracker/core.clj
@@ -1,12 +1,15 @@
 (ns ttracker.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- time-now
+  "Give back current time according to the formatter given.
+   Default formatter- ISO_LOCAL_TIME : '10:15:30' "
+  ([] (time-now java.time.format.DateTimeFormatter/ISO_LOCAL_TIME))
+  ([formatter]
+   (.format (java.time.LocalDateTime/now) formatter)))
 
 (defn osx-notify
+  "Takes in message, title and shows native OSX desktop notification."
   ([message, title]           (osx-notify message title "")) ; blank subtitle
   ([message, title, subtitle] (osx-notify message title subtitle "Glass"))
   ([message, title, subtitle, sound-name]
@@ -21,3 +24,12 @@
              "\" subtitle \""          subtitle
              "\" sound name \""        sound-name
              "\"")])))))
+
+(defn -main
+  "entry point: parses args and calls next function."
+  [pomo-min & tags]
+  (do
+    (println (str "Session started at " (time-now)))
+    (Thread/sleep (* 60 1000 (Integer/parseInt pomo-min)))
+    (println (str "Session ended at   " (time-now)))
+    (osx-notify pomo-min "Time's up")))
--- test/ttracker/core_test.clj
+++ test/ttracker/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [ttracker.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-main
+  (is (-main 12 12)
+      1))
+
+(deftest test-osx-notify
+  (testing "Notification on OSX"
+    (is (let [process (osx-notify "Test body" "Test heading" "test sub")]
+          (.waitFor process))
+        0)))
